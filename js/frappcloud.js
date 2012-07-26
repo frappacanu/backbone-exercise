5c74ef86aa14e9c28d18390a3448e1ff4fd9d449
Francesca Canu <francesca.canu@xing.com>
2012-07-27 01:03:37 +0200

    load more functionality added to search

--- js/frappcloud.js
+++ js/frappcloud.js
@@ -3,8 +3,9 @@ var FrappCloud = Backbone.View.extend({
   
   events: {
     "submit #create-playlist": "createPlaylist",
-    "submit #search": "searchTracks",
-    "click #tabs a": "switchSection"
+    "submit #search": "submitSearch",
+    "click #tabs a": "switchSection",
+    "click #load-more": "searchTracks"
   },
 
   initialize: function() {
@@ -15,13 +16,14 @@ var FrappCloud = Backbone.View.extend({
     this.form = $("#create-playlist");
     this.title = this.form.find("input[name=title]");
     this.description = this.form.find("textarea[name=description]");
+    this.resultsContainer = this.$('#search-results');
     Tracks.fetch();
     Playlists.bind('reset', this.appendAllPlaylists, this);
     Playlists.bind('add', this.appendPlaylist, this);
     Playlists.fetch();
-
+    this.offset = 0;
   },
-  
+
   render: function() {
     this.addAll();
   },
@@ -40,20 +42,30 @@ var FrappCloud = Backbone.View.extend({
     event.preventDefault();
   },
 
-  searchTracks: function(event) {
+  searchTracks: function() {
     var SearchResults = new TrackCollection;
-    this.$("#search-results").html('');
-    SC.get('/tracks', { q: this.searchTerm.val(), limit: 10 }, function(tracks) {
+    SC.get('/tracks', { q: this.searchTerm.val(), limit: 10, offset: this.offset }, _.bind(function(tracks) {
       if (tracks.length < 1) {
         $("#search-results").append("<li>No results</li>");
         return;
       }
       SearchResults.add(tracks);
-      SearchResults.each(function(track){
+      SearchResults.each(_.bind(function(track) {
         var view = new TrackAsSearchResultView({model: track});
-        $("#search-results").append(view.render().el);
-      });
-    });
+        this.resultsContainer.append(view.render().el);
+      }, this));
+      if (this.offset == 0) {
+        this.resultsContainer.after($('<a href="#load-more" class="box load-more" id="load-more">Load More</a>'));
+      }
+      this.offset += 10;
+    }, this));
+  },
+
+  submitSearch: function(event) {
+    this.offset = 0;
+    this.resultsContainer.html('');
+    this.searchTracks();
+    $("#load-more").remove();
     event.preventDefault();
   },
 
@@ -66,9 +78,3 @@ var FrappCloud = Backbone.View.extend({
     event.preventDefault();
   }
 });
-
-
-
-
-
-
